d8c801c6345bff7180c6801909ee11925e14cdfe
Davyd Kohut <davyd.kohut@gmail.com>
2021-10-01 22:47:41 +0300

    3.0.6

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,31 @@
 import { App, reactive } from "vue";
-import { CafeResultV3, CategoryResultV3, DishResultV3, MenuResultV3, TagResultV3 } from "./types";
+import {
+    AdvertisementResultV3,
+    CafeResultV3,
+    CategoryResultV3,
+    DishResultV3,
+    MenuResultV3,
+    TagResultV3
+} from "./types";
 
 export * from "./types";
 
 export interface StorinkaOptions {
     apiUrl?: string;
+    apiVersion?: string;
+    domain?: string;
+}
+
+export class ApiError {
+    code: number;
+    name: string;
+    message: string;
+
+    constructor(code: number, name: string, message: string) {
+        this.code = code;
+        this.name = name;
+        this.message = message;
+    }
 }
 
 export class Storinka {
@@ -18,10 +39,27 @@ export class Storinka {
 
     constructor(options: StorinkaOptions) {
         this.options = options;
+        if (!this.options.apiUrl) {
+            this.options.apiUrl = "https://api.storinka.menu"
+        }
+        if (!this.options.apiVersion) {
+            this.options.apiVersion = "3";
+        }
+        if (!this.options.domain) {
+            this.options.domain = location.hostname;
+        }
 
         this.state = reactive({
             isLoading: false,
         });
+    }
+
+    install(app: App): void {
+        // @ts-ignore
+        app.provide("storinka", this);
+
+        // eslint-disable-next-line no-param-reassign
+        app.config.globalProperties.$storinka = this;
     }
 
     setCafe(id: string, language: string): Promise<CafeResultV3> {
@@ -61,7 +99,7 @@ export class Storinka {
 
     invoke(name: string, params: any = {}): Promise<any> {
         return fetch(
-            `${this.options.apiUrl}/invoke/3/${name}`,
+            `${this.options.apiUrl}/invoke/${this.options.apiVersion}/${name}`,
             {
                 method: "post",
                 headers: {
@@ -71,16 +109,17 @@ export class Storinka {
                 body: JSON.stringify(params),
             },
         )
+            .then(async r => {
+                if (!r.ok) {
+                    const response = await r.json();
+
+                    throw new ApiError(r.status, response.error, response.message);
+                }
+
+                return r;
+            })
             .then((r) => r.json())
             .then((r) => r.result);
-    }
-
-    install(app: App): void {
-        // @ts-ignore
-        app.provide("storinka", this);
-
-        // eslint-disable-next-line no-param-reassign
-        app.config.globalProperties.$storinka = this;
     }
 
     getMenu(menuId: number): MenuResultV3 | undefined {
@@ -147,13 +186,41 @@ export class Storinka {
         return this.state.cafe.tags
             .filter((tag: TagResultV3) => tag.dishes_ids.includes(dishId));
     }
+
+    getAppUrl(path: string): string {
+        if (this.isCustomDomain()) {
+            return `${this.options.domain}/${path}`;
+        }
+
+        return `${this.options.domain}/${this.state.id}/${path}`
+    }
+
+    getMenuUrl(menu: MenuResultV3) {
+        return this.getAppUrl(`${this.getItemProperId(menu)}`)
+    }
+
+    getCategoryUrl(menu: MenuResultV3, category: CategoryResultV3) {
+        return this.getAppUrl(`${this.getItemProperId(menu)}/${this.getItemProperId(category)}`)
+    }
+
+    getDishUrl(menu: MenuResultV3, category: CategoryResultV3, dish: DishResultV3) {
+        return this.getAppUrl(`${this.getItemProperId(menu)}/${this.getItemProperId(category)}/${this.getItemProperId(dish)}`)
+    }
+
+    getItemProperId(item: MenuResultV3 | CategoryResultV3 | DishResultV3 | TagResultV3 | AdvertisementResultV3) {
+        return item.slug || item.hash_id || item.id;
+    }
+
+    isCustomDomain(domain?: string) {
+        if (!domain) {
+            domain = this.options.domain || "";
+        }
+
+        return !(["storinka.menu", "storinka.delivery"].includes(domain));
+    }
 }
 
-const defaultCreateStorinkaOptions: StorinkaOptions = {
-    apiUrl: "https://api.storinka.menu",
-};
-
-export function createStorinka(options: StorinkaOptions = defaultCreateStorinkaOptions): Storinka {
+export function createStorinka(options: StorinkaOptions = {}): Storinka {
     return new Storinka(options);
 }
 
--- src/types.ts
+++ src/types.ts
@@ -1,119 +1,131 @@
 export interface CafeResultV3 {
-  id: number;
-  hash_id: string;
-  update_version: number;
-  name: string;
-  logo?: null | string;
-  cover?: null | string;
-  description?: null | string;
-  slug?: null | string;
-  domain?: null | string;
-  settings: CafeSettingsResultV3;
-  extensions: CafeExtensionsResultV3;
-  menus: Array<MenuResultV3>;
-  categories: Array<CategoryResultV3>;
-  dishes: Array<DishResultV3>;
-  sets: Array<SetResultV3>;
-  options: Array<OptionResultV3>;
-  tags: Array<TagResultV3>;
-  discounts: Array<DiscountResultV3>;
-  advertisements: Array<AdvertisementResultV3>;
-  languages: Array<string>;
+    id: number;
+    hash_id: string;
+    update_version: number;
+    name: string;
+    logo?: null | string;
+    cover?: null | string;
+    description?: null | string;
+    slug?: null | string;
+    domain?: null | string;
+    settings: CafeSettingsResultV3;
+    extensions: CafeExtensionsResultV3;
+    menus: Array<MenuResultV3>;
+    categories: Array<CategoryResultV3>;
+    dishes: Array<DishResultV3>;
+    sets: Array<SetResultV3>;
+    options: Array<OptionResultV3>;
+    tags: Array<TagResultV3>;
+    discounts: Array<DiscountResultV3>;
+    advertisements: Array<AdvertisementResultV3>;
+    languages: Array<string>;
 }
 
 export interface CafeSettingsResultV3 {
-  default_language: string;
-  currency: string;
-  skin: string;
+    default_language: string;
+    currency: string;
+    skin: string;
 }
 
 export interface CafeExtensionsResultV3 {
-  orders?: null | any;
-  cart?: null | any;
-  feedback?: null | any;
+    orders?: null | any;
+    cart?: null | any;
+    feedback?: null | any;
 }
 
 export interface MenuResultV3 {
-  id: number;
-  name: string;
-  description?: null | string;
-  categories_ids: Array<number>;
+    id: number;
+    hash_id: string;
+    slug?: null | string;
+    name: string;
+    description?: null | string;
+    categories_ids: Array<number>;
 }
 
 export interface CategoryResultV3 {
-  id: number;
-  name: string;
-  description?: null | string;
-  dishes_ids: Array<number>;
+    id: number;
+    hash_id: string;
+    slug?: null | string;
+    name: string;
+    description?: null | string;
+    dishes_ids: Array<number>;
 }
 
 export interface DishResultV3 {
-  id: number;
-  name: string;
-  description?: null | string;
-  ingredients?: null | string;
-  image?: null | string;
-  preparing_time?: null | number;
-  variants: Array<DishVariantResultV3>;
-  options: Array<DishOptionResultV3>;
-  settings: any;
+    id: number;
+    hash_id: string;
+    slug?: null | string;
+    name: string;
+    description?: null | string;
+    ingredients?: null | string;
+    image?: null | string;
+    preparing_time?: null | number;
+    variants: Array<DishVariantResultV3>;
+    options: Array<DishOptionResultV3>;
+    settings: any;
 }
 
 export interface DishVariantResultV3 {
-  id: number;
-  name: string;
-  price: number;
+    id: number;
+    name: string;
+    price: number;
 }
 
 export interface DishOptionResultV3 {
-  id: number;
-  option_id: number;
-  type: string;
-  min_items?: null | number;
-  max_items?: null | number;
+    id: number;
+    option_id: number;
+    type: string;
+    min_items?: null | number;
+    max_items?: null | number;
 }
 
 export interface SetResultV3 {
-  id: number;
-  name: string;
-  description?: null | string;
-  price?: null | number;
-  dishes_ids: Array<number>;
-  categories_ids: Array<number>;
+    id: number;
+    hash_id: string;
+    slug?: null | string;
+    name: string;
+    description?: null | string;
+    price?: null | number;
+    dishes_ids: Array<number>;
+    categories_ids: Array<number>;
 }
 
 export interface OptionResultV3 {
-  id: number;
-  name: string;
-  items: Array<OptionItemResultV3>;
+    id: number;
+    name: string;
+    items: Array<OptionItemResultV3>;
 }
 
 export interface OptionItemResultV3 {
-  id: number;
-  name: string;
-  price: number;
+    id: number;
+    name: string;
+    price: number;
 }
 
 export interface TagResultV3 {
-  id: number;
-  label: string;
-  color: string;
-  dishes_ids: Array<number>;
+    id: number;
+    hash_id: string;
+    slug?: null | string;
+    label: string;
+    color: string;
+    dishes_ids: Array<number>;
 }
 
 export interface DiscountResultV3 {
-  id: number;
-  type: string;
-  value: number;
-  round?: null | string;
-  tag_id?: null | number;
-  included_dishes_ids: Array<number>;
+    id: number;
+    type: string;
+    value: number;
+    round?: null | string;
+    tag_id?: null | number;
+    included_dishes_ids: Array<number>;
 }
 
 export interface AdvertisementResultV3 {
-  id: number;
-  title: string;
-  short: string;
-  full?: null | string;
-  color: string;
+    id: number;
+    hash_id: string;
+    slug?: null | string;
+    title: string;
+    short: string;
+    full?: null | string;
+    color: string;
 }
